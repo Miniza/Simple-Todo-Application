a052d4bb7adede8ce3aebd720b0dad46e6c80538
Minenhle <67018029+Miniza@users.noreply.github.com>
2021-11-19 13:28:09 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+const {useState} = React;
+
+const App = () => {
+  return(
+  <ListComp />
+  )
+}
+
+const AddForm = ({addValue}) => {
+const [value, setValue] = useState("")
+const [count, setCount] = useState(0);
+
+const handleSubmit = e => {
+  e.preventDefault();
+  value && addValue(value);
+  setCount(count+1);
+  setValue("");
+}
+  
+  return(
+  <form onSubmit={handleSubmit}>
+   <h4>Number of items on the list: {count}</h4>
+   <input type="text" value={value} onChange = {(e=>setValue(e.target.value))} />
+   <button type="submit">Add</button>
+  </form>
+  ); 
+}
+
+  const ListComp = () => {
+  const [list, setList] = useState([]);
+
+  
+  const addValue = text => setList([...list, { text }]);
+  
+  return(
+  <>
+    <AddForm addValue = {addValue} />
+    <ul>{list.map(item=>{return(<li key={item.index}>{item.text}</li>)})}</ul>
+      
+  </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
